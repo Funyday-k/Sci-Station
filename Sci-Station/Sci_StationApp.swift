f368137f3d7e40578b0d8bfa6e834815d814b653
Xia Lingyu (Funyday-k) <lingyuxia@link.cuhk.edu.hk>
2026-04-27 18:02:37 +0800

    Initial Commit

---
+++ Sci-Station/Sci_StationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sci_StationApp.swift
+//  Sci-Station
+//
+//  Created by Funyday on 2026/4/27.
+//
+
+import SwiftUI
+
+@main
+struct Sci_StationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
